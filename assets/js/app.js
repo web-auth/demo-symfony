d7b1b495dbb6a348631187d5280d75f2337f9c61
Spomky <florent@morselli.fr>
2020-06-10 22:44:03 +0200

    Webauthn Registration

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
-import {useLogin} from "webauthn-helper";
+import {useLogin, useRegistration} from "webauthn-helper";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
@@ -17,4 +17,9 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 window.loginWithWebauthn = useLogin({
     actionUrl: '/webauthn/login',
     optionsUrl: '/webauthn/login/options'
+})
+
+window.registerWithWebauthn = useRegistration({
+    actionUrl: '/webauthn/register',
+    optionsUrl: '/webauthn/register/options'
 })
