06d772ea7475889ea2a345b3697bd038a75ca19e
Spomky <florent@morselli.fr>
2020-06-10 22:04:05 +0200

    Webauthn Authentication On Login Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,14 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import {useLogin} from "webauthn-helper";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+window.loginWithWebauthn = useLogin({
+    actionUrl: '/webauthn/login',
+    optionsUrl: '/webauthn/login/options'
+})
